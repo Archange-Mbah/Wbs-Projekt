2b4dfa885e9d8dc623756f052dd00397a64686d6
Archange Mbah Tchategouang <archange.mbah.tchategouang@mni.thm.de>
2025-02-28 16:27:12 +0100

    before update

--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {AuthService} from '../services/auth.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-auth',
@@ -8,5 +11,11 @@ import { Component } from '@angular/core';
   styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
+
+  username!:string;
+  password!:string;
+  errorMsg!:string;
+  ispasswordValid!:boolean;
+  loginForm: any;
 
 }
